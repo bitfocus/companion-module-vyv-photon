15293ea2b0731b7a0d20b36aad552c8b1191eb1a
Per Røine <per@trippelm.no>
2018-07-13 11:08:33 +0200

    Initial support for VYV Photon Media server

---
+++ HELP.md
@@ -0,0 +1,9 @@
+**VYV Photon**
+
+Module to control the VYV Photon Media server go over to [VYV](https://www.vyv.ca/products/photon/) site to find out more about the software.
+
+We currently support
+
+Recall Cue, which apparently is all you would ever need on this media server.
+
+For additional actions please raise a feature request at [github](https://github.com/bitfocus/companion)
---
+++ index.js
@@ -0,0 +1,149 @@
+var tcp = require('../../tcp');
+var instance_skel = require('../../instance_skel');
+var debug;
+var log;
+
+function instance(system, id, config) {
+	var self = this;
+
+	// super-constructor
+	instance_skel.apply(this, arguments);
+
+	self.actions(); // export actions
+
+	return self;
+}
+
+instance.prototype.updateConfig = function(config) {
+	var self = this;
+
+	self.config = config;
+	self.init_tcp();
+};
+
+instance.prototype.init = function() {
+	var self = this;
+
+	debug = self.debug;
+	log = self.log;
+
+	self.status(1,'Connecting'); // status ok!
+
+	self.init_tcp();
+};
+
+instance.prototype.init_tcp = function() {
+	var self = this;
+
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+		delete self.socket;
+	}
+
+	if (self.config.host) {
+		self.socket = new tcp(self.config.host, 55503);
+
+		self.socket.on('status_change', function (status, message) {
+			self.status(status, message);
+		});
+
+		self.socket.on('error', function (err) {
+			debug("Network error", err);
+			self.status(self.STATE_ERROR, err);
+			self.log('error',"Network error: " + err.message);
+		});
+
+		self.socket.on('connect', function () {
+			self.status(self.STATE_OK);
+			debug("Connected");
+		})
+
+		self.socket.on('data', function (data) {});
+	}
+};
+
+
+// Return config fields for web config
+instance.prototype.config_fields = function () {
+	var self = this;
+	return [
+		{
+			type: 'textinput',
+			id: 'host',
+			label: 'Target IP',
+			width: 6,
+			regex: self.REGEX_IP
+		}
+	]
+};
+
+// When module gets deleted
+instance.prototype.destroy = function() {
+	var self = this;
+
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+	}
+
+	debug("destroy", self.id);;
+};
+
+
+instance.prototype.actions = function(system) {
+	var self = this;
+
+	self.system.emit('instance_actions', self.id, {
+		'cue_exec':    {
+			 label: 'Recall (cue)',
+			 options: [
+				 {
+					type: 'textinput',
+					label: 'Cue ID',
+					id: 'cue',
+					regex: self.REGEX_NUMBER
+				 }
+			 ]
+		 }
+	});
+};
+
+instance.prototype.action = function(action) {
+	var self = this;
+	var cmd
+	var opt = action.options
+
+	switch (action.action){
+
+		case 'powerOn':
+			cmd = '<photon> CUE_EXEC '+ opt.cue + ' </photon>';
+			break;
+
+	};
+
+
+
+
+	if (cmd !== undefined) {
+
+		debug('sending ',cmd,"to",self.config.host);
+
+		if (self.socket !== undefined && self.socket.connected) {
+			self.socket.send(cmd + "\r");
+		} else {
+			debug('Socket not connected :(');
+		}
+
+	}
+
+	// debug('action():', action);
+
+};
+
+instance.module_info = {
+	label: 'VYV Photon',
+	id: 'vyvphoton',
+	version: '0.0.1'
+};
+
+instance_skel.extendedBy(instance);
+exports = module.exports = instance;
